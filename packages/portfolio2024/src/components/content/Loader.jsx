85daf00369a8b2c9e6f5a1281a1c26fe544e65e2
Lee Ye Seo <rosefor2ne@gmail.com>
2024-09-10 23:39:57 +0900

    design: loader icon 삽입

--- packages/portfolio2024/src/components/content/Loader.jsx
+++ packages/portfolio2024/src/components/content/Loader.jsx
@@ -1,28 +1,53 @@
+import { useEffect } from "react";
 import { Html } from "@react-three/drei";
 import { useRecoilState } from "recoil";
 import { IsEnteredAtom } from "../../stores";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export default function Loader() {
   const [isEntered, setIsEntered] = useRecoilState(IsEnteredAtom);
 
-  if (isEntered) return null;
-  console.log("loader");
+  if (isEntered) {
+    return null;
+  } else {
+    useEffect(() => {
+      let timeout;
+      timeout = setTimeout(() => setIsEntered(true), 5000);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+    }, [isEntered, setIsEntered]);
+  }
+
   return (
     <Html center>
-      <EnterBtn onClick={() => setIsEntered(true)}>Enter</EnterBtn>
+      <EnterBtn onClick={() => setIsEntered(true)}>
+        <img src="/icons/mainCharacter.svg" alt="rose fortune cookie" />
+        <span>click!</span>
+      </EnterBtn>
     </Html>
   );
 }
 
+const blink = keyframes`
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.4;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
 const EnterBtn = styled.div`
-  display: block;
+  animation: ${blink} 1.2s infinite;
+  transition-duration: 0.4s;
   cursor: pointer;
-  border: 1px solid black;
-  border-radius: 1rem;
-  padding: 1rem;
+  text-align: center;
   &:hover {
-    background-color: #fff;
-    color: #dc4f00;
+    animation: none;
   }
 `;
