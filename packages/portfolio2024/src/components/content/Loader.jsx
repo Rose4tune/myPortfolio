bd339b5cce4f8e4094b1ca2d1d432bb2be1dc333
Lee Ye Seo <rosefor2ne@gmail.com>
2024-09-11 17:19:07 +0900

    design: text 애니메이션 추가

--- packages/portfolio2024/src/components/content/Loader.jsx
+++ packages/portfolio2024/src/components/content/Loader.jsx
@@ -24,7 +24,7 @@ export default function Loader() {
     <Html center>
       <EnterBtn onClick={() => setIsEntered(true)}>
         <img src="/icons/mainCharacter.svg" alt="rose fortune cookie" />
-        <span>click!</span>
+        <span>click !</span>
       </EnterBtn>
     </Html>
   );
@@ -33,21 +33,32 @@ export default function Loader() {
 const blink = keyframes`
   0% {
     opacity: 1;
+    margin-top: -.5rem;
   }
   50% {
     opacity: 0.4;
+    margin-top: .5rem;
   }
   100% {
     opacity: 1;
+    margin-top: -.5rem;
   }
 `;
 
 const EnterBtn = styled.div`
-  animation: ${blink} 1.2s infinite;
+  animation: ${blink} 1.3s infinite;
   transition-duration: 0.4s;
   cursor: pointer;
   text-align: center;
+  font-family: "Rammetto One";
+  color: #fff;
   &:hover {
     animation: none;
+  }
+
+  span {
+    display: block;
+    margin-top: 1rem;
+    transform: translateX(-5%);
   }
 `;
