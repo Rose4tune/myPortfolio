35fe6671cf748f56a829cadbfff58abf40db39a5
Lee Ye Seo <rosefor2ne@gmail.com>
2024-10-03 16:30:50 +0900

    design: main character 삽입

--- packages/portfolio2024/src/components/twoDimension/MainRoot.jsx
+++ packages/portfolio2024/src/components/twoDimension/MainRoot.jsx
@@ -3,6 +3,7 @@ import { IsEnteredAtom } from "../../stores";
 import Loader from "../loader/Loader";
 import Intro from "./Intro";
 import About from "./About";
+import styled from "styled-components";
 
 export default function MainPage() {
   const isEntered = useRecoilValue(IsEnteredAtom);
@@ -12,8 +13,20 @@ export default function MainPage() {
       <>
         <Intro />
         <About />
+        <MenuBtn>
+          <img src="/icons/mainCharacter.svg" alt="rose's fortune cookie" />
+        </MenuBtn>
       </>
     );
   }
   return <Loader isCompleted />;
 }
+
+const MenuBtn = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 15vw;
+  max-width: 195px;
+  transform: translate(-50%, -50%);
+`;
