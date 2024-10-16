3e6a8bc74aa43c7269764cd0e4d41362ed11d28a
Lee Ye Seo <rosefor2ne@gmail.com>
2024-10-16 18:17:48 +0900

    refactor: skill 파트 css 및 data 분리

--- packages/portfolio2024/src/components/twoDimension/pages/home/Skills.jsx
+++ packages/portfolio2024/src/components/twoDimension/pages/home/Skills.jsx
@@ -1,48 +1,32 @@
 import React, { forwardRef, useState } from "react";
-import styled from "styled-components";
 import Caption from "./elements/Caption";
-
-const skills = [
-  {
-    title: "react",
-    des: "UX/UI를 고려한 개발이 가능합니다.",
-  },
-  {
-    title: "react",
-    des: "UX/UI를 고려한 개발이 가능합니다.",
-  },
-  {
-    title: "react",
-    des: "UX/UI를 고려한 개발이 가능합니다.",
-  },
-  {
-    title: "react",
-    des: "UX/UI를 고려한 개발이 가능합니다.",
-  },
-  {
-    title: "react",
-    des: "UX/UI를 고려한 개발이 가능합니다.",
-  },
-];
+import { skills } from "../../../../data/skills";
+import { useRecoilValue } from "recoil";
+import { LanguageAtom } from "../../../../stores";
 
 const Skills = forwardRef((props, ref) => {
+  const language = useRecoilValue(LanguageAtom);
+  const [selectedTab, setSelectedTab] = useState("front");
   const [flippedCards, setFlippedCards] = useState(
-    // Array(6).fill(false) // 6개의 카드가 모두 초기 상태에서 뒤집히지 않음
-    Array(skills.length).fill(false)
+    Array(skills[selectedTab]).fill(false)
   );
 
-  const handleCardClick = (index) => {
+  const handleTabClick = (tab) => {
+    setSelectedTab(tab);
+  };
+
+  const handleCardClick = (i) => {
     setFlippedCards((prevState) => {
       const newFlippedState = [...prevState];
-      newFlippedState[index] = !newFlippedState[index]; // 토글 기능 구현
+      newFlippedState[i] = !newFlippedState[i];
       return newFlippedState;
     });
   };
 
   return (
-    <Wrap ref={ref}>
-      <Board className="frameShadow_left">
-        {skills.map((skill, i) => (
+    <div ref={ref} className="skills">
+      <ul className="frameShadow_left skills-board">
+        {skills[selectedTab].map(({ title, des }, i) => (
           <li
             key={i}
             className={`${flippedCards[i] ? "flipped" : ""}`}
@@ -50,186 +34,35 @@ const Skills = forwardRef((props, ref) => {
           >
             <div className="front">
               <div className="imgWrap">
-                <img src="/icons/skills/react.svg" alt="react" />
+                <img src={`/icons/skills/${title}.png`} alt={title} />
               </div>
             </div>
             <div className="back">
-              <div className="title">{`${skill.title} /`}</div>
-              <div className="des">{skill.des}</div>
+              <div className="title">{`${title} /`}</div>
+              <div className="des">{des[language]}</div>
             </div>
           </li>
         ))}
-      </Board>
-      <SkillBoard className="tripleLayered">
-        <SkillList>
-          <li>Front-end</li>
-          <li>Back-end</li>
-          <li>Design</li>
-        </SkillList>
+      </ul>
+      <div className="tripleLayered skills-menubar">
+        <ul className="skills-menus">
+          {Object.keys(skills).map((key, i) => {
+            console.log(key);
+            return (
+              <li
+                key={`skillMenu${i}`}
+                className={selectedTab === key ? "active" : ""}
+                onClick={() => handleTabClick(key)}
+              >
+                {key}
+              </li>
+            );
+          })}
+        </ul>
         <Caption type="code" />
-      </SkillBoard>
-    </Wrap>
+      </div>
+    </div>
   );
 });
-
-const Wrap = styled.section`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 4vw;
-`;
-
-const Board = styled.ul`
-  position: relative;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 3vw;
-  width: 70vw;
-  height: 47vw;
-  padding: 15rem 20rem;
-  margin: 0 0 4rem 4rem;
-  background: hsl(var(--gray-back-100));
-  perspective: 50rem;
-  overflow-y: auto;
-  scrollbar-width: thin;
-  border: 1px solid hsl(var(--pink-back-070));
-  z-index: 50;
-
-  li {
-    width: 13vw;
-    height: 13vw;
-    background: hsl(var(--pink-back-090));
-    border: 1px solid hsl(var(--pink-back-070));
-    word-break: break-all;
-
-    position: relative;
-    transform-style: preserve-3d;
-    transform: rotateY(0deg);
-    transition: .5s;
-
-    .front {
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      backface-visibility: hidden;
-
-      &:hover {
-        background: hsl(var(--pink-back-070));
-      }
-    }
-
-    .imgWrap {
-      width: 70%;
-      height: 70%;
-    }
-
-    .back {
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      padding: 1.5rem;
-      backface-visibility: hidden;
-      transform: rotateY(180deg);
-
-      .title {
-        padding-bottom: 1rem;
-        margin-bottom: 1rem;
-        border-bottom: 2px solid hsl(var(--pink-back-070));
-        font-family: 'Noto Sans';
-        font-weight: 800;
-        text-transform: uppercase;
-
-        &::before {
-          display: inline-block;
-          content: url(/icons/arrow_black.svg);
-          transform: translate(-2px, 2px);
-        }
-        &::after {
-          display: inline-block;
-          content: url(/icons/arrow_black.svg);
-          transform: translate(2px, 2px) rotateY(180deg);
-        }
-      }
-      .des {
-        padding: .5rem;
-        font-size: 1.8rem;
-        line-height: 1.2;
-      }
-    }
-
-    &.flipped {
-      transform: rotateY(-180deg) translateZ(-15rem);
-      filter: none;
-      z-index: 100;
-      box-shadow: 1rem 1rem 1.5rem hsla(var(--pink-back-070), .5);
-      background: hsl(var(--gray-back-100));
-
-      &:hover {
-        background: hsl(var(--pink-back-100));
-      }
-    }
-  }
-`;
-
-const SkillBoard = styled.div`
-  width: 15vw;
-  height: 20vw;
-  margin-bottom: 5rem;
-  background: url(/images/skill-bg.svg)no-repeat center center;
-  background-size: contain;
-  border: none;
-
-  &::before {
-    width: 90%;
-    height: 90%;
-    top: 6rem;
-    left: -2rem;
-    background: hsl(var(--pink-back-100));
-  }
-  &::after {
-    width: 90%;
-    height: 90%;
-    top: 7.5rem;
-    left: -3.5rem;
-    background: hsl(var(--pink-back-100));
-  }
-
-  .caption {
-    position: absolute;
-    text-align: left;
-    bottom: -9rem;
-    left: -3rem;
-    
-    &-link {
-      margin-left: 0;
-    }
-  }
-`;
-
-const SkillList = styled.ul`
-  width: calc(100% - 6rem);
-  height: 78%;
-  margin: 19% 3rem;
-  line-height: 1.6;
-  font-size: 3rem;
-  font-weight: 600;
-  color: hsl(var(--pink-fore-030));
-
-  li {
-    cursor: pointer;
-    
-    &::before {
-      display: inline-block;
-      content: "*";
-      margin-right: .5rem;
-      transform: translateY(.5rem);
-    }
-  }
-`;
 
 export default Skills;
---
+++ packages/portfolio2024/src/data/skills.js
@@ -0,0 +1,327 @@
+export const skills = {
+  front: [
+    {
+      title: "css",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "100",
+    },
+    {
+      title: "javascript",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "100",
+    },
+    {
+      title: "typescript",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "60",
+    },
+    {
+      title: "react",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "70",
+    },
+    {
+      title: "next",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "three",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "60",
+    },
+    {
+      title: "farmer motion",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "40",
+    },
+    {
+      title: "gsap",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "70",
+    },
+    {
+      title: "recoil",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "socket.io",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "styled components",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "90",
+    },
+    {
+      title: "tailwind",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "80",
+    },
+  ],
+
+  back: [
+    {
+      title: "supabase",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+  ],
+
+  infra: [
+    {
+      title: "cypress",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "git",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "github actions",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "github",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "storybook",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "swc",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "axios",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "vercel",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "vite",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "vitest",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "webpack",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+    {
+      title: "zustand",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "50",
+    },
+  ],
+
+  design: [
+    {
+      title: "figma",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "100",
+    },
+    {
+      title: "photoshop",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "100",
+    },
+    {
+      title: "illustrator",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "100",
+    },
+    {
+      title: "xd",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "100",
+    },
+    {
+      title: "indesign",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "100",
+    },
+    {
+      title: "sketch",
+      des: {
+        ko: "",
+        en: "",
+      },
+      per: "80",
+    },
+  ],
+};
+
+// {
+//   title: "drei",
+//   des: {
+//     ko: "",
+//     en: "",
+//   },
+//   per: "50",
+// },
+// {
+//   title: "eslint",
+//   des: {
+//     ko: "",
+//     en: "",
+//   },
+//   per: "50",
+// },
+// {
+//   title: "fiber",
+//   des: {
+//     ko: "",
+//     en: "",
+//   },
+//   per: "50",
+// },
+
+// {
+//   title: "pnpm",
+//   des: {
+//     ko: "",
+//     en: "",
+//   },
+//   per: "50",
+// },
+// {
+//   title: "prettier",
+//   des: {
+//     ko: "",
+//     en: "",
+//   },
+//   per: "50",
+// },
+// {
+//   title: "pwa",
+//   des: {
+//     ko: "",
+//     en: "",
+//   },
+//   per: "50",
+// },
+// {
+//   title: "react query",
+//   des: {
+//     ko: "",
+//     en: "",
+//   },
+//   per: "50",
+// },
+// {
+//   title: "react router dom",
+//   des: {
+//     ko: "",
+//     en: "",
+//   },
+//   per: "50",
+// },
