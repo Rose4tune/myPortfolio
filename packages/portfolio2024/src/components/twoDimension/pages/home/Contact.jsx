c272046b90bc17517f29db8181ce881ac9920bdd
Lee Ye Seo <rosefor2ne@gmail.com>
2024-10-21 16:18:20 +0900

    refactor: contact 함수 및 style 리팩토링

--- packages/portfolio2024/src/components/twoDimension/pages/home/Contact.jsx
+++ packages/portfolio2024/src/components/twoDimension/pages/home/Contact.jsx
@@ -1,5 +1,4 @@
 import { forwardRef, useRef, useEffect } from "react";
-import styled from "styled-components";
 import Caption from "./elements/Caption";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -13,56 +12,56 @@ const Contact = forwardRef((props, ref) => {
   const coffeeTextRef = useRef(null);
   const supRef = useRef(null);
 
-  useEffect(() => {
+  const hideText = (textRef, delay) => {
+    gsap.to(textRef.current, {
+      x: "-20%",
+      y: "40%",
+      scale: 0.5,
+      opacity: 0.1,
+      duration: delay,
+      onComplete: () => {
+        textRef.current.style.display = "none";
+      },
+    });
+  };
+
+  const animateElements = () => {
     const delay = 1;
 
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: milkRef.current,
         start: "top 75%",
-        toggleActions: "play none none reset",
+        toggleActions: "play none none none",
       },
     });
 
-    tl.add(() => {
-      gsap.delayedCall(delay, () => {
-        milkTextRef.current.style.display = "none";
-      });
-    }, delay)
-      .add(() => {
-        gsap.delayedCall(delay, () => {
-          coffeeTextRef.current.style.display = "none";
-        });
-      }, delay)
+    tl.add(() => hideText(milkTextRef, delay), delay)
+      .add(() => hideText(coffeeTextRef, delay), delay)
       .fromTo(
         milkRef.current,
-        { opacity: 0, scale: 0.8 },
-        { opacity: 1, scale: 1, duration: 1, delay }
+        { opacity: 0, scale: 0.9 },
+        { opacity: 1, scale: 1, duration: 0.5, delay }
       )
       .fromTo(
         coffeeRef.current,
-        { opacity: 0, scale: 0.8 },
-        { opacity: 1, scale: 1, duration: 1 },
+        { opacity: 0, scale: 0.9 },
+        { opacity: 1, scale: 1, duration: 0.5 },
         "<"
       )
       .fromTo(supRef.current, { y: 0 }, { y: "-30%", duration: 1 }, "<");
+  };
 
-    ScrollTrigger.create({
-      trigger: milkRef.current,
-      start: "top 75%",
-      onLeaveBack: () => {
-        milkTextRef.current.style.display = "block";
-        coffeeTextRef.current.style.display = "block";
-      },
-    });
+  useEffect(() => {
+    animateElements();
   }, []);
 
   return (
-    <Section ref={ref}>
-      <Wrap>
-        <Energe className="font_rampartOne">
+    <section className="contact" ref={ref}>
+      <div className="contact-wrap">
+        <div className="energe font_rampartOne">
           E =&nbsp;
-          <MilkCoffee>
+          <div className="milkCoffee">
             <div className="milk">
               <div className="milk-text" ref={milkTextRef}>
                 m
@@ -86,125 +85,25 @@ const Contact = forwardRef((props, ref) => {
                 <p>coffee</p>
               </div>
             </div>
-          </MilkCoffee>
-          <sup ref={supRef}>2</sup>
-        </Energe>
-        <Connect>
-          <div className="contact-title">
+          </div>
+          <sup ref={supRef} className="energe-sup">
+            2
+          </sup>
+        </div>
+        <div className="connect">
+          <div className="connect-title">
             C &nbsp;O &nbsp;N &nbsp;T &nbsp;A &nbsp;C &nbsp;T
           </div>
           <Caption type="email" lang="en" />
           <Caption type="git" lang="en" />
           <Caption type="blog" lang="en" />
-          <p className="contact-closeGreeting font_ruslanDisplay">
+          <p className="connect-closeGreeting font_ruslanDisplay">
             I LOVE ALL THE CONVERSATIONS.
           </p>
-        </Connect>
-      </Wrap>
-    </Section>
+        </div>
+      </div>
+    </section>
   );
 });
-
-const Section = styled.section`
-  width: 100vw;
-  height: 100vh;
-  background: linear-gradient(
-    180deg,
-    hsl(var(--pink-back-080)) 0%,
-    hsl(var(--gray-back-100)) 100%
-  ), url(/images/contact-bg.png) no-repeat center center;
-  background-size: cover;
-  background-blend-mode: hard-light;
-`;
-
-const Wrap = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 5rem;
-`;
-
-const Energe = styled.div`
-  display: flex;
-  height: 26.4rem;
-  font-size: 18rem;
-  color: hsl(var(--gray-back-100));
-  filter: drop-shadow(.4rem .4rem 0 hsla(var(--pink-back-050), .5));
-
-  sup {
-    font-size: 9rem;
-    transform: translateY(-2rem);
-  }
-`;
-
-const MilkCoffee = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: baseline;
-  gap: 2rem;
-  text-align: center;
-
-  .milk, .coffee {
-    width: 16.5rem;
-    [class$=-icon] {
-      line-height: .8;
-      font-size: 4rem;
-      p::first-letter {
-        font-size: 9rem;
-      }
-    }
-    [class$=-imgWrap] {
-      width: 11rem;
-      margin: 0 auto;
-    }
-  }
-
-  .xIcon {
-    width: 4rem;
-    height: 6rem;
-    background: url(/icons/X.svg) no-repeat center center;
-    transform: translateX(1rem);
-  }
-`;
-
-const Connect = styled.div`
-  padding: 8.3vh 5.2vw;
-  background: hsla(var(--gray-back-100), .6);
-  text-align: center;
-
-  .contact {
-    &-title {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      gap: 2rem;
-      width: fit-content;
-      margin-bottom: 5rem;
-      font-size: 4.8rem;
-      text-transform: uppercase;
-      
-      &::before, &::after {
-        display: block;
-        content: "";
-        width: 7rem;
-        border-top: 2px solid hsl(var(--gray-fore-100));
-      }
-    }
-    &-closeGreeting {
-      margin-top: 5rem;
-      font-family: "Ruslan Display";
-      font-size: 1.5rem;
-    }
-  }
-
-  .caption {
-    position: static;
-    margin: .8rem 0;
-    font-size: 2rem;
-    text-align: center;
-  }
-`;
 
 export default Contact;
