db2785a17766d486a11c6d7dcc502d31d87b3440
Lee Ye Seo <rosefor2ne@gmail.com>
2024-10-24 22:15:12 +0900

    refactor: about data 분리

---
+++ packages/portfolio2024/src/data/about.js
@@ -0,0 +1,65 @@
+export const introduction = {
+  ko: [
+    { key: "목표", value: "브랜드의 가치 상승" },
+    { key: "지향점", value: "기술과 디자인의 융합" },
+    { key: "열정 포인트", value: "최적의 사용자 경험 제공" },
+    { key: "노력 포인트", value: "유지보수 좋은 코드 개발" },
+    { key: "요즘 관심사", value: "웹 3D 인터렉티브" },
+    {
+      key: "요즘 취미",
+      value: "클라이밍, 독서토론, \n전시회, 영화, 여행",
+    },
+    { key: "좋아하는 것", value: "\n디자인, 토론, 물놀이, 남친" },
+  ],
+  en: [
+    { key: "Goals", value: "brand value growth" },
+    { key: "Aim", value: "harmony of tech and design" },
+    { key: "Passion", value: "Provides optimal user experience" },
+    { key: "Effort", value: "Good code for maintenance" },
+    { key: "Interests", value: "Web 3D Interactive" },
+    {
+      key: "Hobbies",
+      value: "Bouldering, Reading, \nExhibitions, Movies, Travel",
+    },
+    {
+      key: "Favorites",
+      value: "\nDesign, Discussion,\nWater play, Boyfriend",
+    },
+  ],
+};
+
+export const keywords = {
+  ko: "넘치는 열정과\n지치않는 에너지,\n끊임없는 호기심,\n단단한 체력과\n유연한 생각도\n능력이다",
+  en: "PASSION\nENERGY\nCURIOSITY\nPHYSICAL\nMENTAL\nSTRENGTH",
+};
+
+export const information = [
+  {
+    title: "* DESIGN SYSTEM",
+    content: {
+      ko: "디자인과 개발간의 시스템을 체계화하여 팀간의 협력과 이후 코드의 유지보수까지 고려한 개발을 지향합니다.",
+      en: "I aim to systematize the system between design and development, taking into account cooperation between teams and the maintenance of the code afterwards.",
+    },
+  },
+  {
+    title: "* OPTIMIZATION",
+    content: {
+      ko: "최적화 툴 Performance, lighthouse 등을 사용하여 렌더링 성능을 고려한 개발을 합니다. 다방면에서 사용자를 고려하여 최적의 경험을 제공하고자 합니다.",
+      en: "I use optimization tools Performance and lighthouse to develop things that take into account rendering performance. And I want to provide the best experience in consideration of users in many ways.",
+    },
+  },
+  {
+    title: "* STUDY",
+    content: {
+      ko: "프론트 외에 디자인과 데이터에 대한 지식에도 관심이 많으며, 블로그나 노션에 정리하고 기록하여 성장하는 습관을 만들어가고 있습니다.",
+      en: "In addition to the front, I am also interested in design and data knowledge, and I am creating a habit of growing by organizing and recording it in my blog or commentary.",
+    },
+  },
+  {
+    title: "* CO-WORK",
+    content: {
+      ko: "함께 일하는 동료 및 유관 부서와의 협업을 중요하게 생각합니다. 서로의 생각과 의도를 온전히 이해할 수 있도록 항상 소통하며 작업합니다.",
+      en: "I value collaboration with colleagues and related departments I work with. Therefore, I always communicate and work so that I can fully understand each other's thoughts and intentions.",
+    },
+  },
+];
