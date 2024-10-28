fa54601281fdb050daa695fc3a93ced6eb98e605
Lee Ye Seo <rosefor2ne@gmail.com>
2024-10-28 19:20:31 +0900

    refactor: caption 데이터 분리

---
+++ packages/portfolio2024/src/data/captions.js
@@ -0,0 +1,79 @@
+export const captions = {
+  resume: {
+    text: {
+      en: "I Want To Stick To The Basics",
+      ko: "언제나 기본부터 충실하고자 합니다.",
+    },
+    linkText: {
+      en: "MY RESUME IS HERE",
+      ko: "이력서 보러가기",
+    },
+    isDiv: true,
+  },
+  study: {
+    text: {
+      en: "I Want To Enjoy The Growth Of Knowledge",
+      ko: "개발과 지식의 성장을 즐기고자 합니다.",
+    },
+    linkText: {
+      en: "MY STUDY IS HERE",
+      ko: "공부 기록 보러가기",
+    },
+    isDiv: true,
+  },
+  code: {
+    text: {
+      en: "I Want To Record Everything Meticulously",
+      ko: "모든 것을 꼼꼼하게 기록하고자 합니다.",
+    },
+    linkText: {
+      en: "MY CODE IS HERE",
+      ko: "코드 기록 보러가기",
+    },
+    isDiv: true,
+  },
+  more: {
+    text: {
+      en: "Are You",
+      ko: "",
+    },
+    linkText: {
+      en: "MORE CURIOUS ABOUT THIS?",
+      ko: "이 프로젝트가 더 궁금하신가요?",
+    },
+    isDiv: false,
+  },
+  email: {
+    text: {
+      en: "If You Want To Talk To Me,",
+      ko: "저와 얘기하고 싶으시다면,",
+    },
+    linkText: {
+      en: "SEND ME AN EMAIL",
+      ko: "메일을 보내주세요.",
+    },
+    isDiv: false,
+  },
+  git: {
+    text: {
+      en: "Curious About My Code,",
+      ko: "저의 코트가 궁금하시다면,",
+    },
+    linkText: {
+      en: "GO GITHUB",
+      ko: "깃헙을 방문해보세요.",
+    },
+    isDiv: false,
+  },
+  blog: {
+    text: {
+      en: "Want To Know What I Study,",
+      ko: "무엇을 공부하는지 궁금하시다면,",
+    },
+    linkText: {
+      en: "GO BLOG",
+      ko: "블로그를 방문해주세요.",
+    },
+    isDiv: false,
+  },
+};
