5c77f402fb77c69fc6b410c347d48578472670fa
Lee Ye Seo <rosefor2ne@gmail.com>
2024-11-07 00:08:27 +0900

    fix: project 스크롤 수정

--- packages/portfolio2024/src/components/twoDimension/pages/home/Projects.jsx
+++ packages/portfolio2024/src/components/twoDimension/pages/home/Projects.jsx
@@ -24,7 +24,7 @@ const Projects = forwardRef((props, ref) => {
         end: "bottom top",
         scrub: true,
         onUpdate: (self) => {
-          const index = Math.floor(self.progress * projects.length - 1);
+          const index = Math.floor(self.progress * projects.length);
           setActiveIndex(index);
         },
       },
